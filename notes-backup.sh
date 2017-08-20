770b0bda319001e7dd44502be2a51e4f97fbc942
Nicolas Bonamy <nicolas@bonamy.fr>
2017-08-20 17:20:50 +0200

    added compression with tar+gzip

--- notes-backup.sh
+++ notes-backup.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 # destination folder
-dest=~/Dropbox/Backup/Notes-$(date +%Y-%m-%d)/
-mkdir -p ${dest}
+dest=~/Dropbox/Backup/Notes-$(date +%Y-%m-%d)
+mkdir -p ${dest}/
 
 # first run export
 echo "Running export of all notes"
-osascript ./notes-export.scpt ${dest}
+osascript ./notes-export.scpt ${dest}/
 
 # now convert with iconv
 echo "Fixing encoding issues"
-for i in ${dest}*
+for i in ${dest}/*
 do
   iconv -f macintosh -t utf-8 < "$i" > "$i.tmp"
   rm "$i"
   mv "$i.tmp" "$i"
 done
+
+# compressing
+echo "Compressing archive"
+tar -C $(dirname ${dest}) -czf ${dest}.tgz $(basename ${dest})/
+rm -r ${dest}/
 
 # done
 echo "Done!"
